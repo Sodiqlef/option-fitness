300eafee76d0266869dfdbe83064951a1a8799d1
sodiqlef <kehindeogundimu731@gmail.com>
2025-07-18 18:39:28 +0100

    add blog models

--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
 # Create your models here.
@@ -12,3 +13,16 @@ class Trainer(models.Model):
 
     def __str__(self):
         return self.name
+
+    
+
+class Blog(models.Model):
+    subject = models.CharField(max_length=255)
+    message = models.TextField(max_length=4000)
+    image = CloudinaryField("image")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return self.name
+    
