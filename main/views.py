ef9cb551d75d77db080f12e12509896c80197761
sodiqlef <kehindeogundimu731@gmail.com>
2025-07-18 18:55:25 +0100

    migrate blogs model

--- main/views.py
+++ main/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, HttpResponse
-from .models import Trainer
+from .models import Trainer, Blog
 
 # Create your views here.
 
 def home(req):
     trainers = Trainer.objects.all()
-    return render(req, "home.html", {"trainers": trainers})
+    blogs = Blog.objects.all()
+    return render(req, "home.html", {"trainers": trainers, "blogs":blogs})
