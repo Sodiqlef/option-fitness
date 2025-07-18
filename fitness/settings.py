16060eeadd1c498c218323b6d7630cd41a392a38
sodiqlef <kehindeogundimu731@gmail.com>
2025-07-18 17:53:27 +0100

    serve static files

--- fitness/settings.py
+++ fitness/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -133,6 +134,9 @@ cloudinary.config(
     api_secret="cl1T1b7f01-08EpbiWO6camIfrQ",
     secure=True
 )
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Set Cloudinary as the default storage for media files
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, HttpResponse
+from .models import Trainer
 
 # Create your views here.
 
 def home(req):
-    return render(req, "home.html")
+    trainers = Trainer.objects.all()
+    return render(req, "home.html", {"trainers": trainers})
